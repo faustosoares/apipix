f2e34852e1b892fc563497b479c7b2bb31f43d4c
Fausto Soares <fausto.ifs@gmail.com>
2025-05-21 20:25:33 -0300

    adicionando security

---
+++ src/main/java/br/com/fbms/apipix/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package br.com.fbms.apipix.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/hello").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user")
+                        .password("{noop}password") // {noop} é para não usar encoding
+                        .roles("USER")
+                        .build()
+        );
+    }
+}
